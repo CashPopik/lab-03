43cea59784000fd2577905bdf26f34845c043bc5
Cash Popik <cashpopik@Cashs-MacBook-Air.local>
2026-09-17 17:22:20 -0600

    Post-lab, works

--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -30,6 +30,8 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
                         cities = cityRepository.cities,
+                        onAddCity = { cityRepository.addCity(it) },
+                        onUpdateCity = { Oldcity, NewCity -> cityRepository.updateCity(Oldcity,NewCity )},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
